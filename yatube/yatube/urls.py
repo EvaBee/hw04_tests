a67ad19dae8fc4d967efd46f819ba8fdc8fb0459
Eva <evel@mail.ru>
2021-07-28 15:08:10 +0300

    some corrections

--- yatube/yatube/urls.py
+++ yatube/yatube/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls import handler404, handler500
 
 from posts.views import page_not_found, server_error
 
-handler404 = "posts.views.page_not_found" # noqa
+handler404 = "posts.views.page_not_found"  # noqa
 handler500 = "posts.views.server_error"  # noqa
 
 
